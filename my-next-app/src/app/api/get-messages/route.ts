c39151f43a1ddaf18c9e1d6aa2c6a879ea159da4
anim200 <126519717+anim200@users.noreply.github.com>
2024-09-14 02:07:10 +0600

    Update route.ts

--- my-next-app/src/app/api/get-messages/route.ts
+++ my-next-app/src/app/api/get-messages/route.ts
@@ -33,7 +33,7 @@ export async function GET(request:Request){
         if (!user || user.length === 0) {
             return Response.json({
                 success:false,
-                message:"User not found"
+                message:"No messages yet"
             },
             {status: 401}
         )
